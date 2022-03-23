5f893d7331aa80386d73d42a9fea134ff2d1afc1
bmsani <bmsani6@gmail.com>
2022-03-24 00:50:13 +0600

    fetch problem solved product are showing

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,18 +1,21 @@
 import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Shop.css'
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        fetch('../fakeData/product.json')
+        fetch('products.json')
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => setProducts(data))
     },[])
     return (
         <div className='shop-container'>
             <div className="products-container">
-                <h3>This is for products: {products.length}</h3>
+                {
+                    products.map(product => <Product product={product} key={product.id}></Product>)
+                }
             </div>
             <div className="cart-container">
                 <h3>order Summary</h3>

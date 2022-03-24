3ae9696af7b297e23ea3dee95056f5263633230e
bmsani <bmsani6@gmail.com>
2022-03-25 00:28:25 +0600

    font awesome used title changed

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 import './Product.css'
 
@@ -15,6 +17,7 @@ const Product = (props) => {
             </div>
             <button onClick={() => handleAddToCart(product)} className='btn-cart'>
                 <p>Add to Cart</p>
+                <FontAwesomeIcon icon={faCartShopping}></FontAwesomeIcon>
             </button>
         </div>
     );

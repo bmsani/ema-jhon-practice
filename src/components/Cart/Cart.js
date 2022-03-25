eb9b4793a42455c76d2aa4995ec43ec6a3bbd1d2
bmsani <bmsani6@gmail.com>
2022-03-25 19:54:50 +0600

    cart data from local storage showing

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,10 +2,22 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = ({cart}) => {
+    let total = 0;
+    let shipping = 0;
+    for (const product of cart) {
+        total = total + product.price
+        shipping = shipping + product.shipping
+    }
+    const tax = total * 0.1;
+    const grandTotal = total + tax + shipping
     return (
-        <div>
+        <div className='cart'>
             <h3>order Summary</h3>
             <p>Selected Items: {cart.length}</p>
+            <p>Total price: ${total} </p>
+            <p>Total Shipping: ${shipping}  </p>
+            <p>Tax: ${tax.toFixed(2)} </p>
+            <h5>Grand Total:  $ {grandTotal} </h5>
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import {addToDb, getStoredCart} from '../../utilities/fakedb'
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -14,9 +15,25 @@ const Shop = () => {
         .then(data => setProducts(data))
     },[])
 
+    useEffect(() => {
+        const storedCart = getStoredCart();
+        const savedCart = []
+        for(const id in storedCart){
+            const addedProduct = products.find(product => product.id === id)
+            if(addedProduct){
+                const quantity = storedCart[id]
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct)
+                console.log(addedProduct);
+            }
+        }
+        setCart(savedCart)
+    },[products])
+
     const handleAddToCart = (product) => {
         const newCart = [...cart, product]
         setCart(newCart)
+        addToDb(product.id)
     }
 
     return (
